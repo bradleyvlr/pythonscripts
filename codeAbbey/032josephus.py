c6f1fb8ac9d039cbb3ea696253049ec89630ad6c
rosalux <bradleyr@cybercon.net>
2018-07-03 18:52:20 -0500

    stuck on josephus. Kinda know what to do but my brain is tired

---
+++ codeAbbey/032josephus.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+ppl = int(input("How many people are to be killed?  - "))
+stp = int(input("How many steps between kills?  - "))
+arr = []
+for a in range(ppl):
+    arr.append(a + 1)
+while len(arr) > 1
+    num += stp
+    med = []
+    if num > len(arr):
+        num -= len(arr)
+        
+    arr.pop(num-1)
+    print(arr)
+    print(num)
+print(arr[0])
